3ebfd686fde7df08e4b256e20701c7f5b83635f8
chenyuhan <yche0705@student.monash.edu>
2026-05-02 20:16:17 +0800

    modify the szie of product card

--- nexmart/app/prod_components/products/ProductCard.tsx
+++ nexmart/app/prod_components/products/ProductCard.tsx
@@ -1,5 +1,6 @@
 // components/products/ProductCard.tsx
 import type { Product } from "@/lib/products";
+import Image from "next/image";
 
 type Props = {
   product: Product;
@@ -7,9 +8,24 @@ type Props = {
 
 export default function ProductCard({ product }: Props) {
   return (
-    <div>
-      <h3>{product.name}</h3>
-      <p>${product.price}</p>
+    <div className="overflow-hidden rounded-2xl border bg-white shadow-sm">
+      
+      {/* image container */}
+      <div className="relative aspect-square w-full bg-slate-100">
+        <Image
+          src={product.image}
+          alt={product.name}
+          fill
+          className="object-cover"
+        />
+      </div>
+
+      {/* text */}
+      <div className="p-4">
+        <h3 className="font-bold">{product.name}</h3>
+        <p className="mt-2 font-bold text-teal-700">${product.price}</p>
+      </div>
+
     </div>
   );
 }
