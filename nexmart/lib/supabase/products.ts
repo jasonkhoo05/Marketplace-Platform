6640dbc78d87243e7a33761e5b890e054556e54f
chenyuhan <yche0705@student.monash.edu>
2026-05-02 19:03:00 +0800

    mock the product data, since the real database haven't been created

---
+++ nexmart/lib/supabase/products.ts
@@ -0,0 +1,32 @@
+//since the database haven't been created, use the mock data
+
+export const products = [
+    {
+      id: 1,
+      name: "Denim Jacket",
+      price: 89,
+      category: "Jacket",
+      image: "/products/DenimJacket.jpg",
+    },
+    {
+      id: 2,
+      name: "Cargo Pants",
+      price: 69,
+      category: "Pants",
+      image: "/products/CargoPants.jpg",
+    },
+    {
+      id: 3,
+      name: "Hoodie",
+      price: 79,
+      category: "Outerwear",
+      image: "/products/hoodle.jpg",
+    },
+    {
+      id: 4,
+      name: "T-shirt",
+      price: 39,
+      category: "T-shirt",
+      image: "/products/T-shirt.jpg",
+    },
+  ];
