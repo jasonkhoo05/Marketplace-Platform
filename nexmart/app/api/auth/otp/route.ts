fcbaf24d2a3cc2d7e539c2af9c8857a3ba69bd9d
Stella Wong Kai Ning <swon0163@student.monash.edu>
2026-05-14 00:31:49 +0800

    feature: initialise post function #backend

--- nexmart/app/api/auth/otp/route.ts
+++ nexmart/app/api/auth/otp/route.ts
@@ -1,3 +1,16 @@
 import { NextResponse} from "next/server";
 import { createClient } from "@/lib/supabase/server";
 
+export async function POST(request: Request) {
+    const {
+        email = await request.json();
+
+        if (!email) {
+            return NextResponse.json(
+                { error: "Email is required"},
+                { status: 400 }
+            )
+        }
+    };
+
+}
