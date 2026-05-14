f1e6a39fffb1d97578b50adbeb196dcb833c3afc
Stella Wong Kai Ning <swon0163@student.monash.edu>
2026-05-14 09:24:08 +0800

    feature: implement forgot password function #frontend

--- nexmart/app/forgot-password/page.tsx
+++ nexmart/app/forgot-password/page.tsx
@@ -1,2 +1,43 @@
 "use client";
 
+import { TbBrandAppgallery } from "react-icons/tb";
+import { useState } from "react";
+
+import "./login.css";
+
+export default function ForgotPasswordPage() {
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [error, setError] = useState("");
+
+
+    const handleForgotPassword = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        setError("");
+        setMessage("");
+
+        try {
+            const res = await fetch("/api/auth/forgot-password", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ email }),
+            });
+
+            const data = await res.json();
+
+            if (!res.ok) {
+                setError(data.error || "Failed to send reset email.");
+                return;
+            }
+
+            setMessage("Password reset email sent. Please check your inbox.");
+        } catch {
+            setError("Something went wrong.");
+        }
+    };
+
+    
+}
