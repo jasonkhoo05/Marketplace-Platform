be55f969d1be4f87df7fbc740764c7637a56f2bd
WanQi-01 <wgoh0026@student.monash.edu>
2026-05-17 18:02:53 +0800

    feature: add the types to be used in page files #backend

--- nexmart/app/admin/types/users.ts
+++ nexmart/app/admin/types/users.ts
@@ -1,34 +1,40 @@
 
 
-// type Address = {
-//     addressId: number;
-//     addressLine: string;
-//     cityLine: string;
-//     postcode: string;
-//     isDefault: boolean;
-//     userUuid: string;
-// };
+export type Address = {
+    // addressId: number;
+    address_line: string;
+    city: string;
+    postcode: string;
+    is_default: boolean;
+    // userUuid: string;
+};
 
-// type UserRole = {
+export type User = {
+    user_uuid: string;
+    username: string;
+    email: string;
+    phone: string | null;
+    last_active_role: string;
+    user_image: string | null;
+    gender: "Male" | "Female" | "Other" | null;
+    date_of_birth: string | null;
+    // userRole?: UserRole[];
+    // address?: Address[];
+};
+
+export type UserDetails = User & {
+    roles: string[];
+    address: Address[];
+};
+
+// export type UserRole = {
 //     roleId: number;
 //     userUuid: string;
 //     role?: Role;
 // };
 
-// type User = {
-//     userUuid: string;
-//     username: string;
-//     email: string;
-//     phone: string | null;
-//     lastActiveRole: string;
-//     userImage: string | null;
-//     gender: "Male" | "Female" | "Other" | null;
-//     dateOfBirth: string | null;
-//     userRole?: UserRole[];
-//     address?: Address[];
-// };
 
-// type Role = {
+// export type Role = {
 //   roleId: number;
 //   roleName: string;
 // };
