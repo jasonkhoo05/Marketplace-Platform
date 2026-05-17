4d6472ea2f837ae74d906cd00baaf0edb01fe52b
WanQi-01 <wgoh0026@student.monash.edu>
2026-05-17 14:15:47 +0800

    refactor: modify the get api #backend

--- nexmart/app/api/admin/users/route.ts
+++ nexmart/app/api/admin/users/route.ts
@@ -3,7 +3,8 @@ import { createClient } from "@/lib/supabase/server";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const limit = parseInt(searchParams.get("limit") || "5", 10);
+  const limitParam = searchParams.get("limit");
+  const limit = limitParam ? parseInt(limitParam , 10) : null;
 
   const supabase = await createClient();
 
@@ -49,10 +50,69 @@ export async function GET(request: Request) {
 
   const users = data ?? [];
 
-  const limitedUsers = users.slice(0, limit);
+  // const limitedUsers = users.slice(0, limit);
+  const resultUsers = limit ? users.slice(0, limit) : users
 
   return NextResponse.json({
-    users: limitedUsers,
+    users: resultUsers,
     totalCount: count || 0,
   });
 }
+
+
+// export async function GET(request: Request) {
+//   const { searchParams } = new URL(request.url);
+//   const limit = parseInt(searchParams.get("limit") || "5" , 10);
+
+//   const supabase = await createClient();
+
+//   const {
+//     data: { user },
+//     error: authError,
+//   } = await supabase.auth.getUser();
+
+//   if (authError || !user) {
+//     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+//   }
+
+//   const { data, error, count } = await supabase
+//     .from("user")
+//     .select(
+//       `
+//       user_uuid,
+//       username,
+//       email,
+//       phone,
+//       last_active_role,
+//       user_image,
+//       gender,
+//       date_of_birth,
+//       address!address_user_uuid_fkey(
+//         address_line,
+//         city,
+//         postcode,
+//         is_default
+//       ),
+//       user_role(
+//         role(
+//           role_name
+//         )
+//       )
+//     `,
+//       { count: "exact" }
+//     );
+
+//   if (error) {
+//     return NextResponse.json({ error: error.message }, { status: 500 });
+//   }
+
+//   const users = data ?? [];
+
+//   const limitedUsers = users.slice(0, limit);
+//   // const resultUsers = limit ? users.slice(0, limit) : users
+
+//   return NextResponse.json({
+//     users: limitedUsers,
+//     totalCount: count || 0,
+//   });
+// }
